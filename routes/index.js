126e636e1aee7e9e63e32401856e2df3c4a1d3be
Rebecca Norén <rebecca.noren@iteam.se>
2020-04-23 16:06:20 +0200

    fixed routing

--- routes/index.js
+++ routes/index.js
@@ -19,25 +19,59 @@ router.get("/sneakers/collection", (req, res) => {
     });
 });
 
-// router.get("/sneakers/collection", (req, res) => {
-//   res.render("products");
-// });
-
 router.get("/sneakers/men", (req, res) => {
-  res.render("products");
+  Sneakers.find({
+      category: "men"
+    })
+    .then((dbResult) => {
+      res.render("products", {
+        sneakers: dbResult,
+        css: ["products.css"],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get("/sneakers/women", (req, res) => {
-  res.render("products");
+  Sneakers.find({
+      category: "women"
+    })
+    .then((dbResult) => {
+      res.render("products", {
+        sneakers: dbResult,
+        css: ["products.css"],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
+router.get("/sneakers/kids", (req, res) => {
+  Sneakers.find({
+      category: "kids"
+    })
+    .then((dbResult) => {
+      res.render("products", {
+        sneakers: dbResult,
+        css: ["products.css"],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 
 router.get("/sneakers/kids", (req, res) => {
   res.render("products");
 });
 
-// router.get("/one-product/:id", (req, res) => {
-//   res.send("baz");
-// });
+router.get("/one-product/:id", (req, res) => {
+  res.send("one_product");
+});
 
 
 router.get("/signin", (req, res) => {
